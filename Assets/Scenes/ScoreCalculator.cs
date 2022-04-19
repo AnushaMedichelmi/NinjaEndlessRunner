e85def3b9f078cfaabcbb694bc38515340028469
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-04-19 14:28:20 +0530

    Game over panel is added.

--- Assets/Scenes/ScoreCalculator.cs
+++ Assets/Scenes/ScoreCalculator.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreCalculator : MonoBehaviour
 {
     // Start is called before the first frame update
     public int score;
     public GameObject winPage;
+    public float time;
+    public GameObject gameOver;
+    public bool isGameWin = false;
+    public Text scoreText;
+
+    private void Update()
+    {
+        scoreText.text = score.ToString();
+        time = time + Time.deltaTime;
+        if ((time > 3f) )
+        {
+            gameOver.SetActive(true);
+        }
+    }
     public void Score(int scoreValue)
     {
         score = score + scoreValue;
         if(score > 30 )
         {
             winPage.SetActive(true);
+            isGameWin = true;
 
         }
         Debug.Log(score);
